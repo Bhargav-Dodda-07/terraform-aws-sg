c5ebac300905c53dde522bb747d6fb6568f58d0b
Bhargav-Dodda-07 <bhargavdodda969@gmail.com>
2026-02-19 13:42:21 +0530

    sg-module

--- main.tf
+++ main.tf
@@ -3,11 +3,18 @@ resource "aws_security_group" "main" {
   description = var.sg_description
   vpc_id      = var.vpc_id
 
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
   tags = merge(
     var.sg_tags,
     local.common_tags,
     {
-        Name = "${local.common_name_suffix}-${var.sg_name}" # roboshop-dev-catalogue
+        Name = "${local.common_name_suffix}-${var.sg_name}"   # roboshop-dev-catalogue
     }
   )
 }
